1b437398a5d9ed339733923afb487b0f096d7f82
alberico fagundes <alberico@fagundes.live>
2023-05-05 11:28:04 -0300

    tratativa aprimorada

--- index.js
+++ index.js
@@ -2,48 +2,81 @@ import { create } from "venom-bot";
 import questions from "./questions.json?v=1" assert { type: "json" };
 
 function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-try {
-  create({
-    session: "chat-gpt",
-    multidevice: true,
+create({
+  session: "WhatsTrivia",
+  multidevice: true,
+})
+  .then((client) => {
+    start(client);
+    console.log("Connected to WhatsTrivia");
   })
-    .then((client) => start(client))
-    .catch((erro) => {
-      console.log(erro);
-    });
-} catch (error) {
-  console.log(error);
-}
+  .catch((error) => {
+    console.log("Error connecting to WhatsApp Business API", error);
+  });
 
 async function start(client) {
   // estado atual do jogo
   let state = {
     questions: [],
-    currentIndex: -1,
+    currentIndex: 0,
     lastWinner: null,
   };
+  console.log("state", state);
 
-  console.log("state",state);
-  client.onAnyMessage( (message) => {
+  function startGame(client, state,message,questions) {
+    console.log("startGame");
+    // Select six random questions from the questions array
+    // const questions = require("./questions.json");
+    const selectedQuestions = shuffle(questions).slice(0, 6);
+
+    console.log("selectedQuestions",selectedQuestions);
+
+    client.sendText(
+      message.from,
+      "Olá! Vamos começar a trivia? A primeira pergunta é:\n\n"
+    );
+
+    // Send the first question to the participant
+    sendQuestion(client, selectedQuestions,state,questions,message);
+  }
+
+  function sendQuestion(
+    client,
+    selectedQuestions,
+    state,   
+    message,
+    questions
+  ) {
+    console.log("sendQuestion");
+    const msg = selectedQuestions[state.currentIndex].questions;
+    client.sendText(message.from, msg);
+    state.currentIndex++;
+  }
+
+  //   state.questions = shuffle(questions).slice(0, 6);
+  //   state.currentIndex = -1;
+  //   state.lastWinner = null;
+
+  client.onAnyMessage((message) => {
+    console.log("message.body", message.body);
     try {
       if (message.body.toLowerCase() === "!trivia") {
-        state.questions = shuffle(questions).slice(0, 6);
-        state.currentIndex = -1;
-        state.lastWinner = null;
-
+        startGame(client, state,message,questions);
         // const selectedQuestions = shuffle(questions).slice(0, 6);
         // console.log("state.questions", state.questions);
-
-         client.sendText(
-            message.from,
-            "Olá! Vamos começar a trivia? A primeira pergunta é:\n\n" +
-              state.questions[0].question
-          );
-          state.currentIndex++;
-
+        // client.sendText(
+        //   message.from,
+        //   "Olá! Vamos começar a trivia? A primeira pergunta é:\n\n" +
+        //     state.questions[0].question
+        // );
+        // state.currentIndex++;
         // message.from é o número do usuário que enviou a msg "hello"
         // client.sendText(message.from, );
       }
