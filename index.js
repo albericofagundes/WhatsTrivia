2d2f7a33ebc04a7b49bde17ea021c3c4cec18050
alberico fagundes <alberico@fagundes.live>
2023-05-12 10:49:33 -0300

    codigo refatorado

--- index.js
+++ index.js
@@ -977,7 +977,7 @@ const questions = [
 
 import { create } from "venom-bot";
 
-let isGameRunning = false;
+// let isGameRunning = false;
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -999,6 +999,33 @@ function updateWinner(state, winnerName) {
   }
 }
 
+async function sendQuestions(client, state, message) {
+  await sleep(1212); // Delay de 3 segundos antes de mostrar a próxima pergunta
+  let question = state.questions[state.currentIndex];
+  await client.sendText(message.from, `${question}`);
+
+  console.log(state.questions[state.currentIndex]);
+  console.log(state.answers[state.currentIndex]);
+}
+
+async function startTriviardy(client, state, message) {
+  state.selectedQuestions = shuffle(questions).slice(0, 6);
+  state.questions = state.selectedQuestions.map((q) => q.question);
+  state.answers = state.selectedQuestions.map((q) => q.answer);
+
+  state.partialAnswers = state.selectedQuestions.flatMap((q) =>
+    q.partialAnswer.map((answer) => cleanString(answer))
+  );
+
+  console.log("state.questions", state.questions);
+  console.log("state.answers", state.answers);
+  console.log("state.partialAnswers ", state.partialAnswers);
+
+  await client.sendText(message.from, `Trivia iniciado`);
+  state.isGameRunning = true;
+  sendQuestions(client, state, message);
+}
+
 // async function sendQuestion(isGameRunning, message, question, answer) {
 //   try {
 //     if (message.body === answer && isGameRunning) {
@@ -1013,6 +1040,35 @@ function updateWinner(state, winnerName) {
 function cleanString(string) {
   return string;
 }
+
+const correctAnswerPromise = new Promise((resolve) => {
+  // Simule a obtenção da resposta correta
+  setTimeout(() => {
+    resolve("Resposta correta");
+  }, 30000);
+});
+
+// Aguardar resposta correta ou tempo expirado
+const answerPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Tempo expirado");
+  }, 20000);
+});
+
+Promise.race([answerPromise, correctAnswerPromise])
+  .then((result) => {
+    console.log("result", result);
+    if (result === "Resposta correta") {
+      // A pessoa acertou a resposta dentro do tempo
+      console.log("Parabéns! Você acertou!");
+    } else {
+      // O tempo expirou
+      console.log("Tempo expirado. Tente novamente.");
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 try {
   create({
@@ -1050,11 +1106,17 @@ function start(client) {
   // async function processAnswer(client, message) {}
 
   client.onAnyMessage(async (message) => {
-    console.log("onAnyMessage isGameRunning", state.isGameRunning);
-    console.log("message", message.body);
-    console.log("state.currentIndex", state.currentIndex);
+    // console.log("onAnyMessage isGameRunning", state.isGameRunning);
+    // console.log("message", message.body);
+    // console.log("state.currentIndex", state.currentIndex);
 
-    if (message.body === "!trivia" && state.isGameRunning === true) {
+    if (message.body == "!endtrivia") {
+      state.isGameRunning = false;
+      console.log("agora terminou");
+      await client.sendText(message.from, `Trivia interrompido`);
+    }
+
+    if (message.body == "!trivia" && state.isGameRunning === true) {
       await client.sendText(
         message.from,
         `Trivia já inicializado, aguarde finalizar`
@@ -1062,34 +1124,11 @@ function start(client) {
     }
 
     if (message.body === "!trivia" && state.isGameRunning === false) {
-      state.selectedQuestions = shuffle(questions).slice(0, 6);
-
-      state.questions = state.selectedQuestions.map((q) => q.question);
-
-      state.answers = state.selectedQuestions.map((q) => q.answer);
+      startTriviardy(client, state, message);
 
       // state.partialAnswers = state.selectedQuestions.map((q) =>
       //   cleanString(q.partialAnswer)
       // );
-
-      state.partialAnswers = state.selectedQuestions.flatMap((q) =>
-        q.partialAnswer.map((answer) => cleanString(answer))
-      );
-
-      console.log("state.questions", state.questions);
-      console.log("state.answers", state.answers);
-      console.log("state.partialAnswers ", state.partialAnswers);
-
-      await client.sendText(message.from, `Trivia iniciado`);
-      await sleep(2000); // Delay de 3 segundos antes de mostrar a próxima pergunta
-
-      let question1 = state.questions[state.currentIndex];
-      await client.sendText(message.from, `${question1}`);
-
-      console.log(state.questions[state.currentIndex]);
-      console.log(state.answers[state.currentIndex]);
-
-      state.isGameRunning = true;
     }
 
     if (
@@ -1113,11 +1152,13 @@ function start(client) {
       // await client.sendText(message.from, `acertou`);
 
       state.currentIndex++;
-      await sleep(2000); // Delay de 3 segundos antes de mostrar a próxima pergunta
+      await sleep(1212); // Delay de 3 segundos antes de mostrar a próxima pergunta
 
-      console.log("state.currentIndex", state.currentIndex);
-      console.log("state.questions", state.questions[state.currentIndex]);
-      console.log("state.answers", state.answers[state.currentIndex]);
+      startTriviardy(client, state, message);
+
+      // console.log("state.currentIndex", state.currentIndex);
+      // console.log("state.questions", state.questions[state.currentIndex]);
+      // console.log("state.answers", state.answers[state.currentIndex]);
       if (
         !(state.currentIndex === state.selectedQuestions.length) &&
         state.isGameRunning == true
