5bce94c4ecde9959dc0e46542c41b74c2618818b
alberico fagundes <alberico@fagundes.live>
2023-05-14 16:01:50 -0300

    questoes de futebol

---
+++ questions-futebol.js
@@ -0,0 +1,661 @@
+questions = [
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2018?",
+    answer: "França",
+    partialanswer: ["França"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador detém o recorde de maior número de gols marcados em Copas do Mundo?",
+    answer: "Marta",
+    partialanswer: ["Marta"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o maior campeão da Liga dos Campeões da UEFA?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador brasileiro foi eleito o melhor do mundo pela FIFA cinco vezes?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual clube tem mais títulos da Copa Libertadores da América?",
+    answer: "Club Atlético Independiente",
+    partialanswer: ["Club Atlético Independiente", "Independiente"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador venceu o prêmio de melhor do mundo da FIFA em 2020?",
+    answer: "Robert Lewandowski",
+    partialanswer: ["Robert Lewandowski", "Lewandowski"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Premier League inglesa?",
+    answer: "Manchester United",
+    partialanswer: ["Manchester United"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção conquistou o maior número de Copas do Mundo?",
+    answer: "Brasil",
+    partialanswer: ["Brasil"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador argentino venceu o prêmio de melhor do mundo da FIFA seis vezes?",
+    answer: "Lionel Messi",
+    partialanswer: ["Lionel Messi", "Messi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Serie A italiana?",
+    answer: "Juventus",
+    partialanswer: ["Juventus"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'O Rei'?",
+    answer: "Pelé",
+    partialanswer: ["Pelé"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2014?",
+    answer: "Alemanha",
+    partialanswer: ["Alemanha"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o estádio com maior capacidade de público do mundo?",
+    answer: "Rungrado 1º de Maio",
+    partialanswer: ["Rungrado 1º de Maio"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa do Brasil?",
+    answer: "Cruzeiro",
+    partialanswer: ["Cruzeiro"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador argentino é conhecido como 'El Pibe'?",
+    answer: "Diego Maradona",
+    partialanswer: ["Diego Maradona", "Maradona"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Copa Libertadores da América no Brasil?",
+    answer: "São Paulo",
+    partialanswer: ["São Paulo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador português é conhecido como 'CR7'?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Liga dos Campeões da UEFA?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador brasileiro venceu o prêmio de melhor do mundo da FIFA três vezes?",
+    answer: "Ronaldo",
+    partialanswer: ["Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2010?",
+    answer: "Espanha",
+    partialanswer: ["Espanha"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador argentino é conhecido como 'La Pulga'?",
+    answer: "Lionel Messi",
+    partialanswer: ["Lionel Messi", "Messi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa do Mundo?",
+    answer: "Brasil",
+    partialanswer: ["Brasil"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Brasileiro?",
+    answer: "Palmeiras",
+    partialanswer: ["Palmeiras"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o estádio conhecido como 'Maracanã'?",
+    answer: "Estádio Jornalista Mário Filho",
+    partialanswer: ["Estádio Jornalista Mário Filho", "Maracanã"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador português venceu o prêmio de melhor do mundo da FIFA em 2016 e 2017?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Bundesliga alemã?",
+    answer: "Bayern de Munique",
+    partialanswer: ["Bayern de Munique"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Ronaldinho Gaúcho'?",
+    answer: "Ronaldinho",
+    partialanswer: ["Ronaldinho"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção conquistou a Copa do Mundo de 2006?",
+    answer: "Itália",
+    partialanswer: ["Itália"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Argentino?",
+    answer: "River Plate",
+    partialanswer: ["River Plate"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'O Fenômeno'?",
+    answer: "Ronaldo",
+    partialanswer: ["Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador italiano venceu o prêmio de melhor do mundo da FIFA em 2006?",
+    answer: "Fabio Cannavaro",
+    partialanswer: ["Fabio Cannavaro", "Cannavaro"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Copa do Mundo de Clubes da FIFA?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o jogador com mais gols marcados em uma única edição da Copa do Mundo?",
+    answer: "Just Fontaine",
+    partialanswer: ["Just Fontaine", "Fontaine"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa América?",
+    answer: "Uruguai",
+    partialanswer: ["Uruguai"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador francês venceu o prêmio de melhor do mundo da FIFA em 2018?",
+    answer: "Luka Modric",
+    partialanswer: ["Luka Modric", "Modric"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Espanhol?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador brasileiro venceu o prêmio de melhor do mundo da FIFA em 2002?",
+    answer: "Ronaldo",
+    partialanswer: ["Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa América?",
+    answer: "Uruguai",
+    partialanswer: ["Uruguai"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa do Brasil?",
+    answer: "Grêmio",
+    partialanswer: ["Grêmio"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador português venceu o prêmio de melhor do mundo da FIFA em 2013 e 2014?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o jogador com mais gols marcados na história da Copa do Mundo?",
+    answer: "Marta",
+    partialanswer: ["Marta"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 1998?",
+    answer: "França",
+    partialanswer: ["França"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Copa Libertadores da América?",
+    answer: "Club Atlético Independiente",
+    partialanswer: ["Club Atlético Independiente", "Independiente"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Zico'?",
+    answer: "Arthur Antunes Coimbra",
+    partialanswer: ["Arthur Antunes Coimbra", "Zico"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Copa do Mundo de Clubes da FIFA?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador argentino venceu o prêmio de melhor do mundo da FIFA em 2020?",
+    answer: "Lionel Messi",
+    partialanswer: ["Lionel Messi", "Messi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa América?",
+    answer: "Uruguai",
+    partialanswer: ["Uruguai"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador espanhol venceu o prêmio de melhor do mundo da FIFA em 2010 e 2012?",
+    answer: "Andrés Iniesta",
+    partialanswer: ["Andrés Iniesta", "Iniesta"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa Intercontinental?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador brasileiro venceu o prêmio de melhor do mundo da FIFA em 2007?",
+    answer: "Kaká",
+    partialanswer: ["Kaká"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Garrincha'?",
+    answer: "Manuel Francisco dos Santos",
+    partialanswer: ["Manuel Francisco dos Santos", "Garrincha"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2014?",
+    answer: "Alemanha",
+    partialanswer: ["Alemanha"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa do Brasil?",
+    answer: "Cruzeiro",
+    partialanswer: ["Cruzeiro"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador argentino venceu o prêmio de melhor do mundo da FIFA em 2019?",
+    answer: "Lionel Messi",
+    partialanswer: ["Lionel Messi", "Messi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Francês?",
+    answer: "Saint-Étienne",
+    partialanswer: ["Saint-Étienne"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador italiano venceu o prêmio de melhor do mundo da FIFA em 1982?",
+    answer: "Paolo Rossi",
+    partialanswer: ["Paolo Rossi", "Rossi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa das Confederações?",
+    answer: "Brasil",
+    partialanswer: ["Brasil"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador espanhol venceu o prêmio de melhor do mundo da FIFA em 2010?",
+    answer: "Iker Casillas",
+    partialanswer: ["Iker Casillas", "Casillas"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Holandês?",
+    answer: "Ajax",
+    partialanswer: ["Ajax"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Romário'?",
+    answer: "Romário de Souza Faria",
+    partialanswer: ["Romário de Souza Faria", "Romário"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador argentino é conhecido como 'La Pulga'?",
+    answer: "Lionel Messi",
+    partialanswer: ["Lionel Messi", "Messi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2006?",
+    answer: "Itália",
+    partialanswer: ["Itália"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Brasileiro?",
+    answer: "Palmeiras",
+    partialanswer: ["Palmeiras"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador francês venceu o prêmio de melhor do mundo da FIFA em 2018?",
+    answer: "Kylian Mbappé",
+    partialanswer: ["Kylian Mbappé", "Mbappé"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Copa do Mundo de Clubes da FIFA?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Ronaldinho Gaúcho'?",
+    answer: "Ronaldo de Assis Moreira",
+    partialanswer: ["Ronaldo de Assis Moreira", "Ronaldinho Gaúcho"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa das Confederações?",
+    answer: "Brasil",
+    partialanswer: ["Brasil"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador português venceu o prêmio de melhor do mundo da FIFA em 2019?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Alemão?",
+    answer: "Bayern de Munique",
+    partialanswer: ["Bayern de Munique"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Ronaldo Fenômeno'?",
+    answer: "Ronaldo Luís Nazário de Lima",
+    partialanswer: ["Ronaldo Luís Nazário de Lima", "Ronaldo Fenômeno"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador argentino é conhecido como 'El Apache'?",
+    answer: "Carlos Tevez",
+    partialanswer: ["Carlos Tevez", "Tevez"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 1998?",
+    answer: "França",
+    partialanswer: ["França"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da UEFA Champions League?",
+    answer: "Real Madrid",
+    partialanswer: ["Real Madrid"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Zico'?",
+    answer: "Arthur Antunes Coimbra",
+    partialanswer: ["Arthur Antunes Coimbra", "Zico"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Argentino?",
+    answer: "River Plate",
+    partialanswer: ["River Plate"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador português venceu o prêmio de melhor do mundo da FIFA em 2008?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa do Mundo?",
+    answer: "Brasil",
+    partialanswer: ["Brasil"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador espanhol venceu o prêmio de melhor do mundo da FIFA em 2010?",
+    answer: "Iker Casillas",
+    partialanswer: ["Iker Casillas", "Casillas"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos do Campeonato Italiano?",
+    answer: "Juventus",
+    partialanswer: ["Juventus"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Didi'?",
+    answer: "Waldyr Pereira",
+    partialanswer: ["Waldyr Pereira", "Didi"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador argentino é conhecido como 'El Kun'?",
+    answer: "Sergio Agüero",
+    partialanswer: ["Sergio Agüero", "Agüero"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual seleção venceu a Copa do Mundo de 2010?",
+    answer: "Espanha",
+    partialanswer: ["Espanha"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Copa do Brasil?",
+    answer: "Cruzeiro",
+    partialanswer: ["Cruzeiro"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Falcão'?",
+    answer: "Paulo Roberto Falcão",
+    partialanswer: ["Paulo Roberto Falcão", "Falcão"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o clube com mais títulos da Liga Europa da UEFA?",
+    answer: "Sevilla",
+    partialanswer: ["Sevilla"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador português venceu o prêmio de melhor do mundo da FIFA em 2019?",
+    answer: "Cristiano Ronaldo",
+    partialanswer: ["Cristiano Ronaldo", "Ronaldo"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual é o país com mais títulos da Copa América?",
+    answer: "Uruguai",
+    partialanswer: ["Uruguai"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual jogador italiano venceu o prêmio de melhor do mundo da FIFA em 2006?",
+    answer: "Fabio Cannavaro",
+    partialanswer: ["Fabio Cannavaro", "Cannavaro"],
+  },
+
+  {
+    category: "futebol",
+    question:
+      "Qual é o clube com mais títulos da Eredivisie (Campeonato Holandês)?",
+    answer: "Ajax",
+    partialanswer: ["Ajax"],
+  },
+
+  {
+    category: "futebol",
+    question: "Qual jogador brasileiro é conhecido como 'Cafu'?",
+    answer: "Marcos Evangelista de Morais",
+    partialanswer: ["Marcos Evangelista de Morais", "Cafu"],
+  },
+];
